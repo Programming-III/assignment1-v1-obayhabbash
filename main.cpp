5842685beb784e59aa9cc562c95df0831602e8b1
obayhabbash <obay.habbash@gmail.com>
2025-11-12 15:24:23 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -7,6 +7,39 @@
 #include <iostream>
 using namespace std;
 
+#include<iostream>
+#include<string>
+using namespace std ;
+animal::animal(){
+private:
+name = "";
+age = 0;
+isHungry=false;
+}
+animal::animal(string name , int age , bool isHungry){
+    this->age;
+    this ->name;
+}
+void display(){
+cout<<name<<age<<isHungary<<endl
+}
+void feed (){
+    if(animal=="hungary")
+        return true ;
+        else return false ;
+    enclosure::enclosure(){
+private:
+capacity=0;
+
+currentCount=0;
+}
+enclosure::enclosure(Dynamic array of animal*,
+int capacity,
+int currentCount){
+}
+
+}
+
 
 // ============== MAIN FUNCTION ==============
 int main() {
