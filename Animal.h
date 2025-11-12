0100be9e168352490ce7ef35ecee67404c772329
obayhabbash <obay.habbash@gmail.com>
2025-11-12 15:14:56 +0200

    Update Animal.h

--- Animal.h
+++ Animal.h
@@ -1,11 +1,24 @@
-#ifndef ANIMAL_H
-#define ANIMAL_H
+#ifndef ANIMAL_H_INCLUDED
+#define ANIMAL_H_INCLUDED
+#include<iostream>
+#include<string>
+using namespace std ;
+ class animal {
+ private :
+    string name ;
+    int age ;
+    bool isHungry;
+ public :
+    animal();
+    animal(string name , int age , bool isHungry);
+    void display();
+    void feed ();
+    ~animal();
+ }
+ 
+            
+    
+ };
 
-#include <string>
-using namespace std;
 
-//define animal class here 
-
-
-
-#endif
+#endif // ANIMAL_H_INCLUDED
