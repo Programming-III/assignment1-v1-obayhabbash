338b38eb179be48fd4f5fda798ef8d013fa47199
obayhabbash <obay.habbash@gmail.com>
2025-11-12 15:23:27 +0200

    Update Visitor.h

--- Visitor.h
+++ Visitor.h
@@ -7,6 +7,21 @@ using namespace std;
 //define visitor class here
 
 
+#ifndef VISITOR_H_INCLUDED
+#define VISITOR_H_INCLUDED
+#include<iostream>
+#include<string>
+class visitor {
+private :
+string visitorName;
+int ticketsBought;}
+public :
+    visitor();
+    visitor(string visitorName,int ticketsBought);
+    void displayinfo();
+    ~visitor();
+
+#endif // VISITOR_H_INCLUDED
 
 
 
