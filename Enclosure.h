7b9c3ff333b2461c9d2c450ba8c5c0f17247b3e5
obayhabbash <obay.habbash@gmail.com>
2025-11-12 15:18:06 +0200

    Update Enclosure.h

--- Enclosure.h
+++ Enclosure.h
@@ -11,5 +11,23 @@
 
 
 
+#ifndef ENCLOSURE_H_INCLUDED
+#define ENCLOSURE_H_INCLUDED
+#include<iostream>
+class enclosure {
+private ;
+Dynamic array of animal*;
+int capacity;
+int currentCount;
+public :
+    enclosure();
+    enclosure(Dynamic array of animal*,int capacity,int currentCount);
+    void addAnimal(Animal*a);
+    void displayAnimals();
+    ~enclosure();
+}
+
+
+#endif // ENCLOSURE_H_INCLUDED
 
 #endif
