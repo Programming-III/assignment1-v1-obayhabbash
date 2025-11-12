2a78d84a38c3909cff264a2fe3ec571c689a67fc
obayhabbash <obay.habbash@gmail.com>
2025-11-12 15:20:31 +0200

    Update Mammal.h

--- Mammal.h
+++ Mammal.h
@@ -7,6 +7,27 @@
 
 
 
+#ifndef MAMMAL_H_INCLUDED
+#define MAMMAL_H_INCLUDED
+#include<iostream>
+#include<string>
+using namespace std ;
+class Mammal:: public animal{
+ private :
+ string name ;
+ int age ;
+ bool isHungry;
+ string furColor;
+ public:
+    Mammal();
+    mammal(string name ,
+ int age ,
+ bool isHungry,
+ string furColor);
+ }
+
+
+#endif // MAMMAL_H_INCLUDED
 
 
 
