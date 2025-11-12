83f67a56043ee0ea80b1a191c5b8807cc751e471
obayhabbash <obay.habbash@gmail.com>
2025-11-12 15:17:08 +0200

    Update Bird.h

--- Bird.h
+++ Bird.h
@@ -6,6 +6,27 @@
 //define class bird here 
 
 
+#ifndef BIRD_H_INCLUDED
+#define BIRD_H_INCLUDED
+#include<iostream>
+#include<string>
+using namespace std;
+class Bird:: public animal{
+ private:
+ string name ;
+ int age ;
+ bool isHungry;
+ float wingSpan;
+ public :
+    Bird();
+    bird(string name ,
+ int age ,
+ bool isHungry,
+ float wingSpan)
+ }
+
+
+#endif // BIRD_H_INCLUDED
 
 
 
