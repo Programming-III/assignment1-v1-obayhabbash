8a38cc54dfa96430ff66e3e66361c167c2a3b5b4
obayhabbash <obay.habbash@gmail.com>
2025-11-12 15:22:42 +0200

    Update Reptile.h

--- Reptile.h
+++ Reptile.h
@@ -7,6 +7,27 @@
 
 
 
+#ifndef REPTILE_H_INCLUDED
+#define REPTILE_H_INCLUDED
+#include<iostream>
+#include<string>
+using namespace std;
+class Reptile:: public animal{
+ private :
+ string name ;
+ int age ;
+ bool isHungry;
+ bool isVenomous;
+ public :
+    Reptile();
+    Reptile(string name ,
+ int age ,
+ bool isHungry,
+ bool isVenomous);
+ }
+
+
+#endif // REPTILE_H_INCLUDED
 
 
 
